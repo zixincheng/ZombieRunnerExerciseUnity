8e0e7699e5ae2bfb272ff4866a90de4838c22c02
zixincheng <jz_zx@hotmail.com>
2016-07-05 12:54:50 -0700

    init commit

---
+++ firstProject/Assets/Scripts/GameObjectUtil.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameObjectUtil : MonoBehaviour {
+
+	// Use this for initialization
+    public static GameObject Instantiate(GameObject prefab, Vector3 pos)
+    {
+        GameObject instance = null;
+
+        instance = GameObject.Instantiate(prefab);
+        instance.transform.position = pos;
+        return instance;
+
+    }
+
+    public static void Destroy(GameObject prefab)
+    {
+
+        GameObject.Destroy(prefab);
+    }
+}
---
+++ firstProject/Assets/Scripts/TiledBackground.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TiledBackground : MonoBehaviour {
+
+    public int textureSize = 32;
+
+    public bool scaleHorizontially = true;
+    public bool scaleVertically = true;
+	// Use this for initialization
+	void Start () {
+        var newWidth = !scaleHorizontially ? 1 : Mathf.Ceil(Screen.width
+            / (textureSize * PixelPerfectCamera.scale));
+
+        var newHieght = !scaleVertically ?  1 : Mathf.Ceil(Screen.height
+            / (textureSize * PixelPerfectCamera.scale));
+
+        transform.localScale = new Vector3(newWidth*textureSize, newHieght*textureSize,1);
+
+        GetComponent<Renderer>().material.mainTextureScale = new Vector3(newWidth, newHieght, 1);
+    }
+	
+	// Update is called once per frame
+
+}
